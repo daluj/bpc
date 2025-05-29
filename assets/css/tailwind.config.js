105a024ea4cdf4d477235b2e536eb15471e7ffd6
daluj <63384857+daluj@users.noreply.github.com>
2025-05-29 22:07:58 +0700

    Initial commit

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,43 @@
+const themeDir = __dirname + '/../../';
+// const defaultTheme = require('tailwindcss/defaultTheme')
+
+const disabledCss = {
+    'code::before': false,
+    'code::after': false,
+    'blockquote p:first-of-type::before': false,
+    'blockquote p:last-of-type::after': false,
+    pre: false,
+    code: false,
+    'pre code': false,
+    'code::before': false,
+    'code::after': false,
+}
+
+module.exports = {
+    prefix: '',
+    content: [
+        `${themeDir}/hugo_stats.json`,
+    ],
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: { css: disabledCss },
+                sm: { css: disabledCss },
+                lg: { css: disabledCss },
+                xl: { css: disabledCss },
+                '2xl': { css: disabledCss },
+            },
+            animation: {
+                'infinite-scroll': 'infinite-scroll 25s linear infinite',
+            },
+            keyframes: {
+                'infinite-scroll': {
+                  from: { transform: 'translateX(0)' },
+                  to: { transform: 'translateX(-100%)' },
+                }
+            }
+        },
+    },
+    variants: {},
+    plugins: [require('@tailwindcss/typography'),require('@tailwindcss/aspect-ratio'),]
+}
---
+++ content/docs/guide/_index.md
@@ -0,0 +1,28 @@
+---
+title: Guide
+weight: 2
+prev: /docs/getting-started
+next: /docs/guide/organize-files
+share_platforms:
+  - platform: facebook
+    url: "https://www.facebook.com/sharer/sharer.php?u="
+    icon: facebook
+  - platform: X
+    url: "https://x.com/intent/tweet?url="
+    icon: x-twitter
+  - platform: Linkedin
+    url: "https://www.linkedin.com/sharing/share-offsite/?url="
+    icon: linkedin
+sidebar:
+  open: true
+---
+
+Explore the following sections to learn how to use Hextra:
+
+<!--more-->
+
+{{< cards >}}
+  {{< card link="organize-files" title="Organize Files" icon="document-duplicate" >}}
+{{< /cards >}}
+
+{{< share-buttons >}}
